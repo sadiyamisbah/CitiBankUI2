2601e9ee67d8e591c41306e330ea5e247b7f3e26
Sadiya Misbah <saadiyamisbah@gmail.com>
2018-06-11 15:42:18 +0530

    Added source code

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function($) {
+   
+ $('.acc-toggle-js-css').click(function(){
+
+        //Expand or collapse
+       $(this).next().slideToggle(400);
+                         
+    }); 
+ 
+});
